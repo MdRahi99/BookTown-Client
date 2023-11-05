2b394fb54ed79e606068d0df85ba206cb27f3466
MD Rahi <fhrstation21@gmail.com>
2023-11-05 13:26:06 +0600

    create admin add book

--- src/Components/Dashboard/AdminDashboard/AddBook/AddBook.jsx
+++ src/Components/Dashboard/AdminDashboard/AddBook/AddBook.jsx
@@ -1,10 +1,53 @@
 import React from 'react';
+import Title from '../../../../Hooks/Title';
+import BookAddForm from '../../../Shared/BookAddForm/BookAddForm';
+import useAuth from '../../../../Hooks/useAuth';
+import useAxiosSecure from '../../../../Hooks/useAxiosSecure';
+import { useForm } from 'react-hook-form';
+import useImageUpload from '../../../../Hooks/useImageUpload';
+import Swal from 'sweetalert2';
 
 const AddBook = () => {
+
+    Title('Add Book');
+
+    const title = 'Add Book';
+
+    const { user } = useAuth();
+    const [axiosSecure] = useAxiosSecure();
+    const { register, handleSubmit, reset } = useForm();
+
+    const { uploadImage } = useImageUpload();
+
+    const onSubmit = async (data) => {
+        const { name, author, rating, price, desc } = data;
+
+        const imgURL = await uploadImage(data.img[0]);
+
+        if (imgURL) {
+            const newItems = { name, email: user?.email, author, rating, price: parseFloat(price), desc, img: imgURL };
+
+            axiosSecure.post('/add-admin-book', newItems)
+                .then(data => {
+                    if (data.data.insertedId) {
+                        reset();
+                        Swal.fire({
+                            title: 'Added Successfully',
+                            icon: 'success',
+                            confirmButtonText: 'Ok'
+                        })
+                    }
+                })
+        }
+    }
+
+
     return (
-        <div>
-            Admin Books
-        </div>
+        <BookAddForm
+            title={title}
+            register={register}
+            handleSubmit={handleSubmit}
+            onSubmit={onSubmit} />
     );
 };
 
