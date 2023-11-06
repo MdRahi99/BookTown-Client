5187b8a0b38d57d67e2341231838e585107985b8
MD Rahi <fhrstation21@gmail.com>
2023-11-06 21:05:49 +0600

    ...

--- src/Components/Dashboard/UserDashboard/MyCart/Payment/Payment.jsx
+++ src/Components/Dashboard/UserDashboard/MyCart/Payment/Payment.jsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import useCart from "../../../../../Hooks/useCart";
 
 const Payment = () => {
+    const [cart] = useCart();
+    const total = cart.reduce((sum, item) => sum + item.price, 0);
+    const price = parseFloat(total.toFixed(2))
     return (
         <div>
-            
+            {price}
         </div>
     );
 };
