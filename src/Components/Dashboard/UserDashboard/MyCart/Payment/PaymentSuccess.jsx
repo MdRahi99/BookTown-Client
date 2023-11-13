653c6784f61bf03d220c3caccb2349118a366988
MD Rahi <fhrstation21@gmail.com>
2023-11-13 12:59:03 +0600

    complete order completed page design

--- src/Components/Dashboard/UserDashboard/MyCart/Payment/PaymentSuccess.jsx
+++ src/Components/Dashboard/UserDashboard/MyCart/Payment/PaymentSuccess.jsx
@@ -15,13 +15,59 @@ const PaymentSuccess = () => {
         setOrder(data.data)
     })
 
-    const { address, category, currency, email, firstName, lastName, name, paid, paidAt, phone, postcode, price, product, transactionId, _id } = order;
+    const { address, category, currency, email, firstName, lastName, name, paid, paidAt, phone, postcode, price, transactionId, _id } = order;
 
     return (
-        <div className='p-8 bg-white rounded-xl'>
-            <h1 className='text-xl font-bold'>Welcome {`${firstName} ${lastName}`}</h1>
-            <button className='py-1 px-3 print:hidden bg-black rounded-lg my-6 hover:bg-orange-600 text-white font-semibold' onClick={() => window.print()}>Print</button>
-        </div>
+        <div className='flex flex-col gap-4 p-8 bg-white rounded-xl'>
+
+            <div className='flex flex-col gap-6'>
+                <h3 className='text-2xl font-semibold font-roboto'>Welcome <span className='text-orange-600'>{`${firstName} ${lastName}`}</span></h3>
+                <p className='text-lg'>Thank you for your purchase. We've received your order. When the order ships, we will send you a email with tracking info.</p>
+                <p className='text-lg font-bold'>Your Order: <span className='border-b-2 border-dotted border-orange-600 p-1'>{transactionId}</span></p>
+            </div>
+
+            <div className="overflow-x-auto my-6">
+                <table className="table w-full">
+                    <thead>
+                        <tr>
+                            <th>Book Name</th>
+                            <th>Qty</th>
+                            <th>Price</th>
+                            <th>Category</th>
+                            <th>Status</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td className='text-lg font-bold'>{name}</td>
+                            <td className='text-lg font-bold'>{1}</td>
+                            <td className='text-lg font-bold'>{currency} {price}</td>
+                            <td className='text-lg font-bold'> {category}</td>
+                            <td className='text-lg font-bold'>{paid && <span className='text-orange-600 font-bold'>Paid</span>}</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+
+            <div className='flex flex-col lg:flex-row p-4 lg:items-center gap-8 lg:gap-0 justify-between'>
+                <div>
+                    <h1 className='text-xl font-bold mb-2 border-b-2 border-black py-1'>Billing Details:</h1>
+                    <p className='text-lg font-medium'>{firstName} {lastName}</p>
+                    <p className='text-lg font-medium'>{phone}</p>
+                    <p className='text-lg font-medium'>{email}</p>
+                </div>
+                <div>
+                    <h1 className='text-xl font-bold mb-2 border-b-2 border-black py-1'>Shipping Details:</h1>
+                    <p className='text-lg font-medium'>{firstName} {lastName}</p>
+                    <p className='text-lg font-medium'>{postcode} {address}</p>
+                    <p className='text-lg font-medium'>{phone}</p>
+                </div>
+            </div>
+
+            <h1 className='text-center my-3'>If you have any questions, please email us: <span className='text-orange-600 font-bold'>support@booktown.com</span></h1>
+
+            <button className='py-1 px-3 w-1/6 mx-auto print:hidden bg-black rounded-lg mt-6 hover:bg-orange-600 text-white font-semibold' onClick={() => window.print()}>Print</button>
+        </div >
     );
 };
 
