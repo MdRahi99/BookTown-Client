b998c79727d9539bdb74fff8d77564e749005087
MD Rahi <fhrstation21@gmail.com>
2023-11-22 18:17:58 +0600

    ...

--- src/Components/Register/Login/Login.jsx
+++ src/Components/Register/Login/Login.jsx
@@ -4,10 +4,12 @@ import Title from '../../../Hooks/Title';
 import Swal from 'sweetalert2';
 import SocialLogin from '../SocialLogin/SocialLogin';
 import useAuth from '../../../Hooks/useAuth';
+import { useState } from 'react';
 
 const Login = () => {
     Title('Login');
     const { signIn, loading } = useAuth();
+    const [error, setError] = useState('');
 
     const location = useLocation();
     const navigate = useNavigate();
@@ -32,7 +34,10 @@ const Login = () => {
                 })
                 navigate(from, { replace: true });
             })
-            .catch(error => console.log(error))
+            .catch(error => {
+                console.log(error)
+                setError(error)
+            })
     };
 
     if (loading) {
@@ -41,34 +46,61 @@ const Login = () => {
 
     return (
         <div>
-            <form onSubmit={handleSignIn} className='bg-slate-100 lg:w-1/2 mx-4 border-l-4 border-dashed border-slate-500 lg:mx-auto p-10 my-10'>
-                <h1 className='font-playFair text-3xl w-full lg:w-3/4 mx-auto shadow-2xl shadow-slate-500 mb-16 border-x-4 border-slate-500 p-4 text-center uppercase'>Sign In</h1>
-                <div className='flex flex-col items-center gap-6'>
-                    <div className='flex items-center shadow-md shadow-slate-500 justify-between gap-3 border-t-2 border-slate-500 text-black p-3 w-96 mx-2 lg:mx-auto'>
-                        <h3 className='text-xl font-playFair'>Email:</h3>
-                        <input name='email' type="email" placeholder="Enter Your Email" className="input input-md font-playFair" />
+            <section className="bg-gray-50 min-h-screen flex items-center justify-center">
+                <div className="bg-gray-100 flex rounded-2xl shadow-lg max-w-3xl p-5 items-center">
+                    <div className="md:w-1/2 px-8 md:px-16">
+                        <h2 className="font-bold text-2xl text-[#002D74]">Login</h2>
+                        <div>
+                            <span>{error}</span>
+                        </div>
+                        <p className="text-xs mt-4 text-[#002D74]">
+                            If you are already a member, easily log in
+                        </p>
+
+                        <form onSubmit={handleSignIn} className="flex flex-col gap-4">
+                            <input
+                                className="p-2 mt-8 rounded-xl border"
+                                type="email"
+                                name="email"
+                                placeholder="Email"
+                            />
+                            <input
+                                className="p-2 rounded-xl border w-full"
+                                type="password"
+                                name="password"
+                                placeholder="Password"
+                            />
+                            <button className="bg-[#002D74] rounded-xl text-white py-2 hover:scale-105 duration-300">
+                                Login
+                            </button>
+                        </form>
+
+                        <div className="mt-6 grid grid-cols-3 items-center text-gray-400">
+                            <hr className="border-gray-400" />
+                            <p className="text-center text-sm">OR</p>
+                            <hr className="border-gray-400" />
+                        </div>
+
+                        <div className='my-6'>
+                            <SocialLogin />
+                        </div>
+
+                        <div className="mt-3 text-xs flex justify-between items-center text-[#002D74]">
+                            <p>Don't have an account?</p>
+                            <Link
+                                to="/register"
+                                className="py-2 px-5 bg-white border rounded-xl hover:scale-110 duration-300"
+                            >
+                                Register
+                            </Link>
+                        </div>
                     </div>
-                    <div className='flex items-center shadow-md shadow-slate-500 justify-between gap-3 border-t-2 border-slate-500 text-black p-3 w-96 mx-2 lg:mx-auto'>
-                        <h3 className='text-xl font-playFair'>Password:</h3>
-                        <input name='password' type="password" placeholder="Enter Your Password" className="input input-md font-playFair" />
-                    </div>
 
-                    <div>
-                        <button className='shadow-lg px-16 py-2 font-playFair text-xl border-x-2 border-slate-500 hover:bg-black hover:text-white'>Log In</button>
-                    </div>
-
-                    <div className="divider font-trainOne animate-bounce">OR</div>
-
-                    <SocialLogin />
-
-                    <div className="flex gap-2 mt-6 justify-center items-center">
-                        <h3>Have not any Account?</h3>
-                        <Link className="text-md font-bold hover:text-slate-500" to="/register">
-                            Sign Up
-                        </Link>
+                    <div className="md:block hidden w-1/2">
+                        <img className="rounded-2xl" src='https://i.ibb.co/Kq7tbWZ/login.jpg' alt="login" />
                     </div>
                 </div>
-            </form>
+            </section>
         </div>
     );
 };
