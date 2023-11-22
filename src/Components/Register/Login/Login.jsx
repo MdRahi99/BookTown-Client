b105c77b2b56d3996edc332c22c9f60787819f6a
MD Rahi <fhrstation21@gmail.com>
2023-11-23 00:29:57 +0600

    ...

--- src/Components/Register/Login/Login.jsx
+++ src/Components/Register/Login/Login.jsx
@@ -46,7 +46,7 @@ const Login = () => {
 
     return (
         <div>
-            <section className="bg-gray-50 min-h-screen flex items-center justify-center">
+            <section className="min-h-full flex items-center justify-center">
                 <div className="bg-gray-100 flex rounded-2xl shadow-lg max-w-3xl p-5 items-center">
                     <div className="md:w-1/2 px-8 md:px-16">
                         <h2 className="font-bold text-2xl text-[#002D74]">Login</h2>
--- src/Components/Register/Register/Register.jsx
+++ src/Components/Register/Register/Register.jsx
@@ -70,7 +70,7 @@ const Register = () => {
 
     return (
         <div>
-            <section className="bg-gray-50 min-h-screen flex items-center justify-center">
+            <section className="min-h-full flex items-center justify-center">
                 <div className="bg-gray-100 flex rounded-2xl shadow-lg max-w-3xl p-5 items-center">
                     <div className="md:w-1/2 px-8 md:px-16">
                         <h2 className="font-bold text-2xl text-[#002D74]">Register</h2>
@@ -130,7 +130,7 @@ const Register = () => {
                     </div>
 
                     <div className="md:block hidden w-1/2">
-                        <img className="rounded-2xl" src='https://i.ibb.co/30Hh28z/register.jpg' alt="login" />
+                        <img className="rounded-2xl h-96" src='https://i.ibb.co/30Hh28z/register.jpg' alt="login" />
                     </div>
                 </div>
             </section>
